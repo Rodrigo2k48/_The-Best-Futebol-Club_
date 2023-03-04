93b661469add2d944f5afe3f8bdc3711b0b81337
Rodrigo <rodrigoga701@gmail.com>
2023-03-04 09:08:07 -0300

    test: TDD - caso o usuario esteja cadastrado no banco de dados, retorna um token

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -2,6 +2,8 @@ import chai from "chai"
 import sinon from "sinon"
 import chaihtpp from 'chai-http'
 import { app } from "../app"
+import { Model } from "sequelize"
+import User from "../../src/database/models/User"
 
 chai.use(chaihtpp)
 const {expect} = chai
@@ -11,7 +13,19 @@ describe("Testes na rota Login na aplicação", async () => {
         sinon.restore()
     })
 it("/login - POST - deve retornar status 200 e um token caso o usuario esteja no banco de dados", async () => {
-    const response = await chai.request(app).post("/login")
+    const loginMock = {
+        username: 'User',
+        role: 'user',
+        email: 'user@user.com',
+        password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO', 
+          // senha: secret_user
+      } as unknown as User
+  const findByPk = sinon.stub(Model, 'findByPk').resolves(loginMock)
+  const response = await chai.request(app).post("/login").send({
+    email: "user@user.com",
+    password: "secret_user",
+  })
     expect(response.status).to.be.equal(200)
+    expect(response.body).to.property("token")
 }) 
 })
