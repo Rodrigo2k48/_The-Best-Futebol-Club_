953b2e9b716e0ccfc52660b9a1e4f2895cba70a1
Rodrigo <rodrigoga701@gmail.com>
2023-03-04 12:00:29 -0300

    fix: resolvendo um erro de sintax - na função onde eu quero mockar no Sinon, no arquivo de testes de intregação da rota login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -13,14 +13,14 @@ describe("Testes na rota Login na aplicação", async () => {
         sinon.restore()
     })
 it("/login - POST - deve retornar status 200 e um token caso o usuario esteja no banco de dados", async () => {
-    const loginMock = {
+    const loginMock = [{
         username: 'User',
         role: 'user',
         email: 'user@user.com',
         password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO', 
           // senha: secret_user
-      } as unknown as User
-  const findByPk = sinon.stub(Model, 'findByPk').resolves(loginMock)
+      }] as unknown as User[]
+  sinon.stub(Model, 'findAll').resolves(loginMock)
   const response = await chai.request(app).post("/login").send({
     email: "user@user.com",
     password: "secret_user",
