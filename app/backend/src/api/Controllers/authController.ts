3f912fe1641bf231be77dd335bde198e7507a7ec
Rodrigo <rodrigoga701@gmail.com>
2023-03-06 16:04:26 -0300

    feat: implementando a validação de caso o usuario não estiver com um email cadastrado no banco de dados e realizar o login na aplicação, um erro deve ser disparado

--- app/backend/src/api/Controllers/authController.ts
+++ app/backend/src/api/Controllers/authController.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import IAuthService from '../interfaces/IAuthService';
+import IUser from '../interfaces/IUser';
 import HttpException from '../shared/HttpException';
 
 export default class AuthController {
@@ -11,11 +12,12 @@ export default class AuthController {
   public async login(req: Request, res: Response, next: NextFunction):
   Promise<Response | void> {
     try {
-      const { email, password } = req.body;
+      const { email, password } = req.body as IUser;
       if (!email || !password) {
         throw new HttpException(400, 'All fields must be filled');
       }
-      const token = await this.service.generateToken({ email, password });
+      const payload = { email, password };
+      const token = await this.service.generateToken(payload);
       return res.status(200).json({ token });
     } catch (error) {
       next(error);
--- app/backend/src/api/Services/authService.ts
+++ app/backend/src/api/Services/authService.ts
@@ -3,6 +3,8 @@ import User from '../../database/models/User';
 import IAuthService from '../interfaces/IAuthService';
 import { IJwtService } from '../shared/interfaces/IJwtToken';
 import IUser from '../interfaces/IUser';
+import ValidateUser from '../middlewares/validateUser';
+import HttpException from '../shared/HttpException';
 
 export default class AuthService implements IAuthService {
   protected model: ModelStatic<User> = User;
@@ -13,10 +15,24 @@ export default class AuthService implements IAuthService {
   }
 
   async generateToken(dto: IUser): Promise<string> {
-    const { email } = dto;
-    const userInDb = await this.model.findAll({ where: { email } });
-    const { role, id } = userInDb[0];
-    const token = this.tokenService.create({ role, id });
+    const { email, password } = dto;
+    const validate = new ValidateUser(email, password);
+    const userInDb = await this.getUserInDb(email);
+    if (!userInDb) {
+      throw new HttpException(401, 'Invalid email or password');
+    }
+    const isValid = await validate.validateUser(userInDb.email, userInDb.password);
+    if (!isValid) {
+      throw new HttpException(401, 'Invalid email or password');
+    }
+    const { id, role } = userInDb;
+    const userInfos = { id, role };
+    const token = this.tokenService.create(userInfos);
     return token;
+  }
+
+  public async getUserInDb(email: string): Promise<User> {
+    const user = await this.model.findAll({ where: { email } });
+    return user[0];
   }
 }
