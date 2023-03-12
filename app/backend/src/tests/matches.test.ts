18781b578bf768f1d49e3367fa91a6247ceab3ee
Rodrigo <rodrigoga701@gmail.com>
2023-03-12 14:08:42 -0300

    test: TDD - deve retornar a lista de todos os jogos

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -2,6 +2,8 @@ import chaiHttp from "chai-http"
 import chai from "chai"
 import Sinon from "sinon"
 import { app } from "../app"
+import Matche from "../database/models/Matche"
+import { Model } from "sequelize"
 
 
 chai.use(chaiHttp)
@@ -12,7 +14,42 @@ describe("testes na rota Matches na aplicação", () => {
         Sinon.restore()
     })
     it("/matches - GET - deve retornar status 200 e a lista de todos os jogos", async () => {
-        const response = await chai.request(app).get("/matches")
-        expect(response.status).to.equal(200)
+        const matcheMock = [
+            {
+              "id": 1,
+              "homeTeamId": 16,
+              "homeTeamGoals": 1,
+              "awayTeamId": 8,
+              "awayTeamGoals": 1,
+              "inProgress": false,
+              "homeTeam": {
+                "teamName": "São Paulo"
+              },
+              "awayTeam": {
+                "teamName": "Grêmio"
+              }
+            },
+            {
+              "id": 41,
+              "homeTeamId": 16,
+              "homeTeamGoals": 2,
+              "awayTeamId": 9,
+              "awayTeamGoals": 0,
+              "inProgress": true,
+              "homeTeam": {
+                "teamName": "São Paulo"
+              },
+              "awayTeam": {
+                "teamName": "Internacional"
+              }
+            }
+          ] as unknown as Matche[]
+      
+        Sinon.stub(Model, 'findAll').resolves(matcheMock)
+      // Action     
+      const response = await chai.request(app).get('/matches')
+      // Assertion
+      expect(response.status).to.equal(200)
+      expect(response.body).to.deep.equal(matcheMock)
     })
 })
