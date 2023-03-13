fd1c75bfa0ba73780436c572a19acce63746240d
Rodrigo <rodrigoga701@gmail.com>
2023-03-13 15:14:55 -0300

    test: TDD - caso o usuario tente atualizar uma partida para finalizada, com um token invalido, um erro é disparado na aplicação

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -137,5 +137,14 @@ describe("testes na rota Matches na aplicação",  async () => {
           expect(res.body).to.have.property('message').equal('Token not found');
           done();
         });
-      })
+    })
+    it("/matches/id/finish- PATCH - deve retonar status 401 e uma mensagem de erro caso o usuario estiver com um token invalido", async () => {
+        const tokenInvalid = "eyJhbGciOiJIUzI1iIsnR5cCI6IkpXVCJ9.eyJpZCI6Miwicm9sZSI6InVzZXIiLCJpYXQiOjE2NzgxMjkyNzYsImV4cCI6MTcyMTMyOTI3Nn0.lIiw4TS_EoQUAgQ1acKCoWVGuBx0PZ6YnCSrMnPYhsw"
+        const response = await chai.request(app).patch("/matches/2/finish").set({ 'Authorization': tokenInvalid });
+        expect(response.status).to.be.equal(401)  
+        expect(response.body).to.deep.equal({
+          "message": "Token must be a valid token"
+        })
+        expect(response.body).to.have.property('message')
+    })
 })
