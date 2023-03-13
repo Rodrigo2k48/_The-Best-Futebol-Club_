ba4b0f3b61f2953a79fde7f77dd78d104e487846
Rodrigo <rodrigoga701@gmail.com>
2023-03-13 15:22:55 -0300

    feat: implementando a validação caso o usuario tente atualizar uma partida para finalizada com um token invalido,um erro é disparado na aplicação

--- app/backend/src/api/middlewares/verifyToken.ts
+++ app/backend/src/api/middlewares/verifyToken.ts
@@ -1,10 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
 import HttpException from '../shared/HttpException';
+import TokenService from '../shared/tokenService';
 
 export default function validationToken(req: Request, res: Response, next: NextFunction) {
   const { authorization } = req.headers;
   if (!authorization) {
     throw new HttpException(401, 'Token not found');
+  }
+  try {
+    const payload = new TokenService().validate(authorization);
+    // pra caso no futuro eu precise das informações do usuario
+    req.body.user = payload;
+    next();
+  } catch (e) {
+    throw new HttpException(401, 'Token must be a valid token');
   }
   next();
 }
