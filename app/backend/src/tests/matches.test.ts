34220803a8920c7ea7a0916534c064773fcb4763
Rodrigo <rodrigoga701@gmail.com>
2023-03-13 10:11:45 -0300

    test: TDD - deve retornar a lista de todos os jogos que já estão finalizados

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -89,5 +89,44 @@ describe("testes na rota Matches na aplicação",  async () => {
       expect(response.body[0].inProgress).to.equal(true)
       expect(response.body[0].id).to.equal(41)
       expect(response.body[0].awayTeam.teamName).to.equal("Internacional")
+    }),
+    it("/matches?inProgress - GET - deve retornar status 200 e a lista dos jogos que já estão finalizados", async () => {
+      const matcheMock = [
+        {
+          "id": 1,
+          "homeTeamId": 16,
+          "homeTeamGoals": 1,
+          "awayTeamId": 8,
+          "awayTeamGoals": 1,
+          "inProgress": false,
+          "homeTeam": {
+            "teamName": "São Paulo"
+          },
+          "awayTeam": {
+            "teamName": "Grêmio"
+          }
+        },
+        {
+          "id": 41,
+          "homeTeamId": 16,
+          "homeTeamGoals": 2,
+          "awayTeamId": 9,
+          "awayTeamGoals": 0,
+          "inProgress": true,
+          "homeTeam": {
+            "teamName": "São Paulo"
+          },
+          "awayTeam": {
+            "teamName": "Internacional"
+          }
+        }
+      ] as unknown as Matche[]
+      Sinon.stub(Model, 'findAll').resolves(matcheMock)
+      const response = await chai.request(app).get('/matches?inProgress=false')
+
+      expect(response.status).to.equal(200)
+      expect(response.body[0].inProgress).to.equal(false)
+      expect(response.body[0].id).to.equal(1)
+      expect(response.body[0].awayTeam.teamName).to.equal("Grêmio")
     })
 })
