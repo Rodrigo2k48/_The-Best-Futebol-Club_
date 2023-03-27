5b77cd41660a66ee4167d33b3242394d804312fd
Rodrigo <rodrigoga701@gmail.com>
2023-03-26 21:52:15 -0300

    test: TDD - deve retornar a lista dos times que não são mandantes e as informações referentes as estatisticas e posição

--- app/backend/src/tests/leaderboards.test.ts
+++ app/backend/src/tests/leaderboards.test.ts
@@ -41,6 +41,32 @@ describe("testes na rota Leaderboards na aplicação", async () => {
           const response = await chai.request(app).get('/leaderboard/home')
       
           expect(response.status).to.be.equal(200); 
-          console.log(response.body);
+         
+    })
+    it(" /leaderboard/away - GET - deve retornar status 200 e a lista dos times que não são times da casa com todas as informações referentes as estatiticas e posição na tabela", async () => {     
+        const teamListMock = [ 
+            { id: 1, teamName: 'Avaí/Kindermann' },
+            { id: 2, teamName: 'Bahia' },
+            { id: 3, teamName: 'Botafogo' },
+            { id: 4, teamName: 'Corinthians' },
+            { id: 5, teamName: 'Cruzeiro' },
+            { id: 6, teamName: 'Ferroviária' },
+            { id: 7, teamName: 'Flamengo' },
+            { id: 8, teamName: 'Grêmio' },
+            { id: 9, teamName: 'Internacional' },
+            { id: 10, teamName: 'Minas Brasília' },
+            { id: 11, teamName: 'Napoli-SC' },
+            { id: 12, teamName: 'Palmeiras' },
+            { id: 13, teamName: 'Real Brasília' },
+            { id: 14, teamName: 'Santos' },
+            { id: 15, teamName: 'São José-SP' },
+            { id: 16, teamName: 'São Paulo' }
+          ]
+
+          Sinon.stub(Model, 'findAll').resolves(teamListMock as Team[]);
+      
+          const response = await chai.request(app).get('/leaderboard/away')
+      
+          expect(response.status).to.be.equal(200); 
     })
 })
