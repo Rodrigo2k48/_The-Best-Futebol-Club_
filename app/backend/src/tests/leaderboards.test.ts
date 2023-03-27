2dc809536937960e2cbb84385741e8bc6114d9a9
Rodrigo <rodrigoga701@gmail.com>
2023-03-26 23:00:08 -0300

    test: TDD - deve retornar todas as informações com todas as estatisticas de todos os times, tanto os times fora de casa quanto os times mandantes

--- app/backend/src/tests/leaderboards.test.ts
+++ app/backend/src/tests/leaderboards.test.ts
@@ -69,4 +69,30 @@ describe("testes na rota Leaderboards na aplicação", async () => {
       
           expect(response.status).to.be.equal(200); 
     })
+    it(" /leaderboard/ - GET - deve retornar status 200 e a lista de todos os times com todas as informações referentes as estatiticas e posição na tabela", async () => {     
+        const teamListMock = [ 
+            { id: 1, teamName: 'Avaí/Kindermann' },
+            { id: 2, teamName: 'Bahia' },
+            { id: 3, teamName: 'Botafogo' },
+            { id: 4, teamName: 'Corinthians' },
+            { id: 5, teamName: 'Cruzeiro' },
+            { id: 6, teamName: 'Ferroviária' },
+            { id: 7, teamName: 'Flamengo' },
+            { id: 8, teamName: 'Grêmio' },
+            { id: 9, teamName: 'Internacional' },
+            { id: 10, teamName: 'Minas Brasília' },
+            { id: 11, teamName: 'Napoli-SC' },
+            { id: 12, teamName: 'Palmeiras' },
+            { id: 13, teamName: 'Real Brasília' },
+            { id: 14, teamName: 'Santos' },
+            { id: 15, teamName: 'São José-SP' },
+            { id: 16, teamName: 'São Paulo' }
+          ]
+
+          Sinon.stub(Model, 'findAll').resolves(teamListMock as Team[]);
+      
+          const response = await chai.request(app).get('/leaderboard/')
+      
+          expect(response.status).to.be.equal(200); 
+    })
 })
