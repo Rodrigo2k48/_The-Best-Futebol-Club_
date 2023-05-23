ee155cdf282a3ff85716d0e5a95dd58099753067
Rodrigo Gonçalves <rodrigoga701@gmail.com>
2023-05-22 23:25:08 -0300

    fix: esqueci de colocar uns expect em "teams.test.ts" nada de tão grande, mas para seguir o padrão dos outros arquivos de testes, é importante

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -43,12 +43,13 @@ describe('GET /teams/:id', () => {
     it("deve retornar o time no banco de dados referente ao id passado e enviar status 200", async () => {
       sinon.stub(Model, 'findByPk').resolves(TEAMS_IN_DB[0]);
   
-      const {status, body} = await chai.request(app).get('/teams/1');
+      const {status, body, ok} = await chai.request(app).get('/teams/1');
       
       expect(status).to.equal(HTTP_STATUS.SuccessOK);
       expect(body).to.have.property('id').equal(TEAMS_IN_DB[0].id);
       expect(body).to.have.property('teamName').equal(TEAMS_IN_DB[0].teamName);
       expect(body).to.deep.equal(TEAMS_IN_DB[0])
+      expect(ok).to.equal(true)
     })
   })
   describe('em caso de erro no banco de dados', () => {
