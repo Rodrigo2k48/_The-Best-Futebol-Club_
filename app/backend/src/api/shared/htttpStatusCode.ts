a9db8e9fddd8a1328a366e07eefaa940ca96602b
Rodrigo Gonçalves <rodrigoga701@gmail.com>
2023-05-24 01:12:15 -0300

    refactor: Refatorei todos os testes da "matches.test.ts" porem o serviço ainda não acabou nesse arquivo, ainda falta os 100 de coverage, que eu só vou conseguir se começar a refatorar e eliminar as funções em desuso no codigo, por isso irei apartir desse commit focar em refatorar as classes e funções no backend da aplicação

--- app/backend/src/api/shared/htttpStatusCode.ts
+++ app/backend/src/api/shared/htttpStatusCode.ts
@@ -1,8 +1,11 @@
 enum HTTP_STATUS {
   SuccessOK = 200,
+  SuccessCreated = 201,
   ClientErrorBadRequest = 400,
   ClientErrorUnauthorized = 401,
-  InternalServerError = 500 
+  UnprocessableContentError = 422,
+  InternalServerError = 500, 
+  NotFoundError = 404,
 }
 
 export default HTTP_STATUS;
