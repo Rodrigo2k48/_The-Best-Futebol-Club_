a2f937229445c36f70582616e961489b169e8ecf
Rodrigo Gonçalves <rodrigoga701@gmail.com>
2023-05-29 19:28:28 -0300

    remove: estou removendo a funcionalidade de buscar as partidas pelo id (motivo: criei essa funcionalidade na matcheService.ts da minha aplicação, para me ajudar em uma determinada logica porem eu consegui resolver sem o auxilio dessa função, fazendo ela não ter mais serventia)

--- app/backend/src/api/interfaces/IMatchesService.ts
+++ app/backend/src/api/interfaces/IMatchesService.ts
@@ -5,7 +5,6 @@ export default interface IMatchesService {
   getAllMatches(): Promise<Matche[]>
   getAllByProgress(progress: string | boolean): Promise<Matche[] | void>
   updateProgressMatch(id: string | number): Promise<string | void>
-  getMacheByID(id: string | number): Promise<Matche>
   updateMatchGoalsById(id: string | number, homeTeamGoals: number, awayTeamGoals: number):
   Promise<string>
   createMatch(dto: IMatch): Promise<Matche | void>
